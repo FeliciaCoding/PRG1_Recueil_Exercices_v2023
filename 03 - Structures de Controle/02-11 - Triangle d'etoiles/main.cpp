bac64e60fa2f6b84c15582552d5a56c736006d4f
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-07-09 22:30:40 +0200

    User input for 02-12 ppmc

--- 03 - Structures de Controle/02-11 - Triangle d'etoiles/main.cpp
+++ 03 - Structures de Controle/02-11 - Triangle d'etoiles/main.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 int main() {
 
     // assuming the high can only be integer
---
+++ 03 - Structures de Controle/02-12 - ppmc/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int m, n;
+    do{
+        cout << "Giving 2 positive integer (non-zero) : ";
+        cin >> m >> n;
+    }while(m<=0 || n<=0 );
+
+
+    cout << "Hello, World!" << endl;
+    return 0;
+}
