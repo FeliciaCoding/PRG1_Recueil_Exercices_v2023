d4f6ad7c039aea9df4306d617711eeafc09ddd87
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-07-09 18:30:45 +0200

    Add 02-11

---
+++ 03 - Structures de Controle/02-11 - Triangle d'etoiles/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    // assuming the high can only be integer
+    int high;
+    do {
+        cout << "Enter the high of the triangle (h > 0): ";
+        cin >> high;
+    } while (high <= 0);
+
+    for (int i = 0; i <high ; ++i){
+        for (int j = 0; j < high+i+1 ; ++j){
+            if ( j < high - i ) {
+                cout << " ";
+            } else {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
