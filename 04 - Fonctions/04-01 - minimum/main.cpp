06b69df831bdbf24ef97b55009b61c138be2068e
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-07-31 21:38:47 +0200

    Complete 04-01

--- 04 - Fonctions/04-01 - minimum/main.cpp
+++ 04 - Fonctions/04-01 - minimum/main.cpp
@@ -1,8 +1,13 @@
-// ne pas utilise de if
+// ne pas utilise de 'if'
 
 #include <iostream>
 
 using namespace std;
+
+// Assignable -> return type: by reference
+int& min(int& a, int& b, int& c);
+
+void printEmptyLine ();
 
 int main() {
     int a = 5;
@@ -10,8 +15,23 @@ int main() {
     int c = 9;
 
     cout << min(a, b, c);
+    printEmptyLine ();
     min(a, b, c) = 1;
     int& d = min(a, b, c);
+    cout << d;
+    printEmptyLine ();
+    cout << "a = " << a << ", b = " << b << ", c= " << c;
+    printEmptyLine ();
 
     return 0;
+}
+
+
+int& min(int& a, int& b, int& c) {
+
+    return a > b ? (b > c ? c : b) : (a > c ? c : a);
+}
+
+void printEmptyLine () {
+    cout << endl;
 }
