a655be8437eebb4773f38b63c115a804beb7b797
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-08-22 20:57:10 +0200

    Add variable e, modify comments

--- 04 - Fonctions/04-02 - minimum/main.cpp
+++ 04 - Fonctions/04-02 - minimum/main.cpp
@@ -20,20 +20,21 @@ int main() {
     // a=12 ,b=7 ,c=9, min=7
 
     int& d = min(a, b, c); // return minValeur(int&)
+    int e = min(a, b, c);
     // ---------------------------------------------------------------//
     // when d is reassigned, min(), minValeur will be changed
     // when minVariable was reassigned directly, min() will be changed but d will remain the same
     // if min(a, b, c) is reassigned, d wont change
     // ---------------------------------------------------------------//
 
-    cout << "d:" << d << endl << endl;
+    cout << "d:" << d <<  " e:" << e << endl << endl;
     //  d=7
 
     b=5; // min = b, will change min and d
     cout << "2) b = 5;" << endl;
     cout << "a:" << a << " b:" << b << " c:" << c
          << " => min:" << min(a, b, c) << " ";
-    cout << "d:" << d << endl << endl;
+    cout << "d:" << d <<  " e:" << e << endl << endl;
     // a=12 ,b=5 ,c=9, min=5,
     // Error d=7 -> 5 (lvalue: int& d = min(a, b, c) = b = 5)
 
@@ -42,18 +43,18 @@ int main() {
     cout << "3) d = 3;" << endl;
     cout << "a:" << a << " b:" << b << " c:" << c
          << " => min:" << min(a, b, c) << " ";
-    cout << "d:" << d << endl << endl;
+    cout << "d:" << d <<  " e:" << e << endl << endl;
     // a=12 ,b=5->3 ,c=9, min=5->3, d=3
 
     // int& d = min(a, b, c) = a or b or c = value = 3
     // Error b=5 -> 3
     // Error min=5 -> 3
 
-    a=1; // WONT CHANGE d, because min was b
+    a=1; // WONT CHANGE d, because d points to b
     cout << "4) a = 1;" << endl;
     cout << "a:" << a << " b:" << b << " c:" << c
          << " => min:" << min(a, b, c) << " ";
-    cout << "d:" << d << endl << endl;
+    cout << "d:" << d <<  " e:" << e << endl << endl;
     // a=1 ,b=5 -> 3 ,c=9, min=1, d=3
 
     cout << "5) min(a, b, c) = 0;" << endl;
@@ -65,11 +66,18 @@ int main() {
     //----------------------------------------------------//
     cout << "a:" << a << " b:" << b << " c:" << c
          << " => min:" << min(a, b, c) << " ";
-    cout << "d:" << d << endl << endl;
-    // a=0 ,b=3 ,c=9, min=0, d=0-> 3 (d is only assigned once in the beginning of the program )
+    cout << "d:" << d <<  " e:" << e << endl << endl;
+    // a=0 ,b=3 ,c=9, min=0, d=0-> 3 (because d points to b )
 
     // int& d = min(a, b, c) = a = value = 0
     // Error d=0 -> 3
+
+    b = 1;
+    cout << "6) b = 1;" << endl;
+    cout << "a:" << a << " b:" << b << " c:" << c
+         << " => min:" << min(a, b, c) << " ";
+    cout << "d:" << d <<  " e:" << e << endl << endl;
+
 
     return EXIT_SUCCESS;
 }
