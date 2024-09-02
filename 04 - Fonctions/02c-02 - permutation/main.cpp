47eaea4ba47805cc849193d8805f5a1b05c76c64
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-09-02 22:55:32 +0200

    complete 02c-02

--- 04 - Fonctions/02c-02 - permutation/main.cpp
+++ 04 - Fonctions/02c-02 - permutation/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+using namespace std;
+
+void permutationCirculaireDroite (double* a, double* b, double* c) {
+
+    double temp;
+
+    temp = *c, *c= *b, *b = *a, *a = temp;
+
+}
+
+void display (double a, double b, double c) {
+
+    cout << "a = " << a << ", b = " << b << ", c = " << c << endl;
+}
+
 int main() {
+
+    double a = 1, b = 2, c = 3;
+
+    for (int i = 0; i < 3; ++i ) {
+
+        // permutationCirculaireDroite(a, b, c);
+        permutationCirculaireDroite(&a, &b, &c);
+
+        display(a, b, c);
+    }
+
+
+
+
+
 
     return 0;
 }
