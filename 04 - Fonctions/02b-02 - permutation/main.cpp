0344249c152c70eec8605946692817aa6ae327b8
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2024-09-02 18:40:01 +0200

    Complete 04-002b-02

--- 04 - Fonctions/02b-02 - permutation/main.cpp
+++ 04 - Fonctions/02b-02 - permutation/main.cpp
@@ -1,8 +1,38 @@
+/*
+ * Ecrire un programme C++ qui met à disposition une fonction
+ * sans valeur de retour et prenant 3 paramètres de type double
+ * permettant d'effectuer une permutation circulaire droite
+ * de ces 3 valeurs.
+ */
+
 #include <iostream>
 
 using namespace std;
 
+void afficher(double& a,double& b,double& c) {
+    cout << "( a = " << a << ", b = " << b << ", c = " << c << " )" <<  endl;
+}
+
+void permutationDroit (double& a,double& b,double& c) {
+    double t ;
+
+    for (int i = 0 ; i < 3 ; ++i){
+        // a = c, b = a, c = b // 3, 3, 3
+        t = c, c = b, b = a, a = t;
+        afficher(a,b,c);
+
+    }
+
+    cout << "End of permutation." << endl;
+}
+
+
+
 int main() {
-    cout << "Hello, World!" << :endl;
+
+    double x = 1, y = 2, z = 3 ;
+
+    permutationDroit(x,y,z);
+
     return 0;
 }
